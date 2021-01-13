a4e8be3a85e6ad5660ac8860e0f82e7c97d5ee87
diegoduenez03 <19170154@uttcampus.edu.mx>
2021-01-13 21:49:38 +0000

    Cambios para los menus y se creo su clase

--- Clases.py
+++ Clases.py
@@ -1,6 +1,7 @@
 registroPedir = {}
 registroDev = {}
 
+
 class Persona:
     
   def pide(self, idPersona, nombre,material, fecha):
@@ -21,7 +22,6 @@ class Persona:
     registroDev.update({'Fecha devolucion '+ repr(idPersona):fecha})
   
       
-
 class Lista:
 
   def pedidos(self, activar):
@@ -45,3 +45,42 @@ class Lista:
       print("          ")
     
 
+class Menu:
+
+  def principal(self):
+    print("- - - MENU PRINCIPAL - - -")
+    print("(1) Persona que pidio material")
+    print("(2) Persona que devolvio material")
+    print("(3) Lista de pedidos")
+    print("(4) Lista de entregas")
+    print("(5) Salir")
+    print("          ")
+
+  
+  def pedido(self, persona = Persona()):
+    print("Ingrese el ID de la persona:")
+    idNew = input()
+    print("Ingrese el nombre de la persona:") 
+    nombreNew = input()
+    print("Ingrese el material que pidio:") 
+    materialNew = input()
+    print("Ingrese la fecha de pedido (DD/MM/AA):")
+    fechaNew = input()
+    print("          ")
+    print("Atencion: La persona " + nombreNew + " pidió " + materialNew)
+    print("          ")
+    persona.pide(idNew, nombreNew, materialNew, fechaNew)
+
+  def devolucion(self, persona = Persona()):
+    print("Ingrese el ID de la persona:")
+    idDev = input()
+    print("Ingrese el nombre de la persona:") 
+    nombre = input()
+    print("Ingrese el material que devolvio:")
+    material = input()
+    print("Ingrese la fecha de devolucion (DD/MM/AA):")
+    fecha = str(input())
+    print("          ")
+    print("Atencion: La persona " + nombre + " devolvio " + material)
+    print("          ")
+    persona.devuelve(idDev, nombre, material, fecha)
--- main.py
+++ main.py
@@ -1,51 +1,22 @@
-from Clases import Persona, Lista
-
+from Clases import Persona, Lista, Menu
 
 salir = False
 lista = Lista()
 persona = Persona()
+menu = Menu()
 
 while True:
   
-  print("- - - MENU PRINCIPAL - - -")
-  print("(1) Persona que pidio material")
-  print("(2) Persona que devolvio material")
-  print("(3) Lista de pedidos")
-  print("(4) Lista de entregas")
-  print("(5) Salir")
-  print("          ")
+  menu.principal()
   mainMenu = int(input())
-  print("          ")
-
+  
   if mainMenu == 1:
-    print("Ingrese el ID de la persona:")
-    idNew = input()
-    print("Ingrese el nombre de la persona:") 
-    nombreNew = input()
-    print("Ingrese el material que pidio:") 
-    materialNew = input()
-    print("Ingrese la fecha de pedido (DD/MM/AA):")
-    fechaNew = input()
-    fechaStr = str(fechaNew)
-    print("          ")
-    print("Atencion: La persona " + nombreNew + " pidió " + materialNew)
-    print("          ")
-    persona.pide(idNew, nombreNew, materialNew, fechaNew)
+    menu.pedido()
     salir = False
 
   if mainMenu == 2:
-    print("Ingrese el ID de la persona:")
-    idDev = input()
-    print("Ingrese el nombre de la persona:") 
-    nombre = input()
-    print("Ingrese el material que devolvio:")
-    material = input()
-    print("Ingrese la fecha de devolucion (DD/MM/AA):")
-    fecha = str(input())
-    print("          ")
-    print("Atencion: La persona " + nombre + " devolvio " + material)
-    print("          ")
-    persona.devuelve(idDev, nombre, material, fecha)
+    menu.devolucion()
+    salir = False
     
   if mainMenu == 3:
     lista.pedidos(True)
@@ -58,7 +29,7 @@ while True:
   if mainMenu == 5:
     print("Has salido del sistema")
     salir = True
-    
+
   if(salir == True):
         break
 
